b1f0c03e5e75a4358ae1f13a14da0ebc396099ff
SixiyevIH <ilqar.sixiyev@kapitalbank.az>
2022-05-16 17:07:52 +0400

    initial commit

---
+++ src/main/java/az/xezer/mssticker/MsStickerApplication.java
@@ -0,0 +1,13 @@
+package az.xezer.mssticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsStickerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/xezer/mssticker/MsStickerApplicationTests.java
@@ -0,0 +1,13 @@
+package az.xezer.mssticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
